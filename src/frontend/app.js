c82497ff72078855ffa8bd933c3051b712723c7f
gonzalomartinez520 <gonzalomartinez520@gmail.com>
2026-04-08 03:07:13 -0300

    Update app.js

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -60,11 +60,11 @@ function inicializarFormulario() {
     const remitente = document.getElementById("remitente")?.value;
     const destinatario = document.getElementById("destinatario")?.value;
 
-    const origenCiudad = document.getElementById("ciudadOrigen")?.value;
+    const origen = document.getElementById("ciudadOrigen")?.value;
     const origenPais = document.getElementById("paisOrigen")?.value;
     const origenDireccion = document.getElementById("direccionOrigen")?.value;
 
-    const destinoCiudad = document.getElementById("ciudadDestino")?.value;
+    const destino = document.getElementById("ciudadDestino")?.value;
     const destinoPais = document.getElementById("paisDestino")?.value;
     const destinoDireccion = document.getElementById("direccionDestino")?.value;
 
